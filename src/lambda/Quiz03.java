4b21b10b08550708951cd94ce079abd748de45f5
JuYoung <jjystar3@gmail.com>
2024-07-26 10:40:33 +0900

    주석 수정

--- src/lambda/Quiz03.java
+++ src/lambda/Quiz03.java
@@ -19,7 +19,7 @@ public class Quiz03 {
 		System.out.println(strConverter2.convert("there"));		
 
 		// 람다식 함수
-		StringConverter strConverter3 = (s) -> s.toUpperCase();
+		StringConverter strConverter3 = s -> s.toUpperCase(); // 매개변수 1개일 때는 () 생략 가능
 		System.out.println(strConverter3.convert("buddy"));		
 
 	}
